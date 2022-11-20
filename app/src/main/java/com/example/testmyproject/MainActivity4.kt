f1d08f95feeb82868c950fb17649e73b23dea125
kaykeguedes <guedeskayke@gmail.com>
2022-11-20 15:47:36 -0300

    Ajustes no layout e implementação de API

--- app/src/main/java/com/example/testmyproject/MainActivity4.kt
+++ app/src/main/java/com/example/testmyproject/MainActivity4.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        val btnInicio = findViewById<Button>(R.id.btnInicio)
+
+        btnInicio.setOnClickListener{
+            val i = Intent (this, MainActivity3::class.java)
+            startActivity(i)
+        }
+
+        val btnInicioIcon = findViewById<ImageButton>(R.id.btnInicioIcon)
+
+        btnInicioIcon.setOnClickListener{
+            val i = Intent (this, MainActivity3::class.java)
+            startActivity(i)
+        }
 
         val btnMapaEcopontos = findViewById<Button>(R.id.btnMapaEcopontos)
 
